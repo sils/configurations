336a396f500259bb322289fac20c2e3a13842f4f
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-07-31 18:56:31 +0200

    Install git configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Install vim configurations
 ln -s $DIR/vim/.vimrc ~/
 ln -s $DIR/vim/.vim ~/
+
+# Install git configurations
+ln -s $DIR/git/.gitconfig ~/
 
