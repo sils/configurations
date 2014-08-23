7ca7ac61f44647e122bdc276d039c6bc53b3b8b0
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-23 18:31:14 +0200

    Make script better

--- scripts/git/collect_authors_patches.sh
+++ scripts/git/collect_authors_patches.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [ ! $# == 1 ]; then
-    echo "Usage: $0 author_name"
+if [ ! $# == 2 ]; then
+    echo "Usage: $0 author_name output_root_directory"
     exit 1
 fi
 
 author=$1
+output=$2
 end=""
 begin=""
+patchcount=1
+rangecount=1
 
 for O in $(git log --pretty=format:"%h" --author="$author"); do
         if ! git log --pretty=format:"%h" --author="$author" $O^^..$O^ --exit-code 1>/dev/null ; then
@@ -15,12 +18,28 @@ for O in $(git log --pretty=format:"%h" --author="$author"); do
                 end=$O
             fi
             begin=$O
+            ((rangecount++))
         else
             if [[ "$end" == "" ]]; then
-                git format-patch $O^..$O
+                tmp=$(($patchcount - 1))
+                echo "Adding one patch to $tmp existing:"
+                git log $O^..$O
+                echo "Please enter a subdirectory name for this patch:"
+                read subdir
+                mkdir -p $output/$subdir
+                git format-patch $O^..$O -o$output/$subdir
+                ((patchcount++))
             else
-                git format-patch $begin^^..$end
+                tmp=$(($patchcount - 1))
+                echo "Adding $rangecount patches to $tmp existing."
+                git log $begin^^..$end --oneline
+                echo "Please enter a subdirectory name for this patch:"
+                read subdir
+                mkdir -p $output/$subdir
+                git format-patch $begin^^..$end -o$output/$subdir
                 end=""
+                patchcount=$(($patchcount + $rangecount))
+                rangecount=1
             fi
         fi
     done
