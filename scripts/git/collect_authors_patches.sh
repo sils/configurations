07fc9db4fd6f95cae8f78425a7ac9f0b6d882410
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-08-23 18:39:26 +0200

    Commentary additions

--- scripts/git/collect_authors_patches.sh
+++ scripts/git/collect_authors_patches.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Copyright 2014 by Lasse Schuirmann <lasse.schuirmann@gmail.com>
+# License: GPL 3
+
 if [ ! $# == 2 ]; then
     echo "Usage: $0 author_name output_root_directory"
+    echo
+    echo "This is a simple helper script that allows collecting all patches from"
+    echo "one author in a git repository. It will detect consequent ranges of"
+    echo "patches and ask for each range for a subdirectory so you can distinguish"
+    echo "the patch series'."
+
     exit 1
 fi
 
@@ -27,7 +36,7 @@ for O in $(git log --pretty=format:"%h" --author="$author"); do
                 echo "Please enter a subdirectory name for this patch:"
                 read subdir
                 mkdir -p $output/$subdir
-                git format-patch $O^..$O -o$output/$subdir
+                git format-patch $O^..$O -o$output/$subdir #--start-number=$patchcount
                 ((patchcount++))
             else
                 tmp=$(($patchcount - 1))
@@ -36,7 +45,7 @@ for O in $(git log --pretty=format:"%h" --author="$author"); do
                 echo "Please enter a subdirectory name for these patches:"
                 read subdir
                 mkdir -p $output/$subdir
-                git format-patch $begin^^..$end -o$output/$subdir
+                git format-patch $begin^^..$end -o$output/$subdir #--start-number=$patchcount
                 end=""
                 patchcount=$(($patchcount + $rangecount))
                 rangecount=1
